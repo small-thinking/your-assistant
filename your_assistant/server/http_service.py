58211bee6142c4286c4cc230bd3a5e5220fbeb52
Yexi Jiang <yjiang@Yexis-MacBook-Air.local>
2023-03-26 20:42:33 -0700

    Add http service

---
+++ your_assistant/server/http_service.py
@@ -0,0 +1,29 @@
+"""Http service for your assistant.
+"""
+from flask import Flask, request
+
+from your_assistant.core.orchestrator import RevChatGPTOrchestrator
+from your_assistant.core.utils import load_env
+
+app = Flask("Your Assistant")
+orchestrator = None
+
+
+def init_service():
+    global orchestrator
+    load_env()
+
+    orchestrator = RevChatGPTOrchestrator()
+
+
+@app.route("/api/v1/assistant", methods=["POST"])
+def handle_request():
+    if request.method == "POST":
+        prompt = request.json["prompt"]
+        response = orchestrator.process(prompt=prompt)
+        return {"response": response}
+
+
+if __name__ == "__main__":
+    init_service()
+    app.run(host="0.0.0.0", port=32167, debug=True)
