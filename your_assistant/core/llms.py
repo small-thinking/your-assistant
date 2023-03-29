a0a2be65880b0c119ae93028cf49ad3786b28455
Yexi Jiang <yjiang@Yexis-MacBook-Air.local>
2023-03-29 00:01:43 -0700

    add bard

--- your_assistant/core/llms.py
+++ your_assistant/core/llms.py
@@ -3,6 +3,7 @@
 import os
 from typing import List, Optional
 
+from Bard import Chatbot as BardChat
 from langchain import PromptTemplate
 from langchain.llms.base import LLM
 from revChatGPT.V1 import Chatbot
@@ -38,3 +39,30 @@ class RevChatGPT(LLM):
         for data in chatgpt.ask(prompt):
             response = data["message"]
         return response
+
+
+class RevBard(LLM):
+    test_mode: bool = False
+
+    @property
+    def _llm_type(self) -> str:
+        return "RevBard"
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        """Call the LLM. In test mode, return a test response.
+
+        Args:
+            prompt (str): The prompt to the LLM.
+            stop (Optional[List[str]]): The stop tokens. Will be ignored.
+
+        Returns:
+            str: The response from the LLM.
+        """
+        response = ""
+        if self.test_mode:
+            response = "This is a test response."
+            return response
+
+        bard = BardChat(session_id=os.getenv("BARD_SESSION_ID"))
+        response = bard.ask(message=prompt)
+        return response["content"]
