e06f941397dabd2c9bd97c70438b46d5882bb424
Jing Fu <jingfu4@gmail.co>
2023-04-07 11:04:25 +0800

    Add audio transcribe endpoint (use openai whisper v1 api)

--- your_assistant/server/http_service.py
+++ your_assistant/server/http_service.py
@@ -1,7 +1,9 @@
 """Http service for your assistant.
 """
+import os
 from argparse import Namespace
 
+import openai
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
@@ -44,6 +46,26 @@ def handle_bard_request():
         args.prompt = prompt
         response = bard_orchestrator.process(args=args)
         return {"response": response}
+
+
+@app.route("/api/v1/audio/transcribe", methods=["POST"])
+def handle_audio_transcribe_request():
+    # request.form is empty (not None) when there is no form data
+    # if model not specified, default to whisper-1; otherwise, use the specified model (only whisper-1 is available as of April 2023)
+    if "model" not in request.form or request.form["model"] == "whisper-1":
+        model = "whisper-1"
+        if "prompt" in request.form:
+            prompt = request.form["prompt"]
+        # TODO(fuj): error handling; secure filename
+        file = request.files["file"]
+        file.save(file.filename)
+        audio_file = open(file.filename, "rb")
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        # TODO(fuj): probably makes more sense to use open-source Whisper model and move this into an core audio class
+        transcript = openai.Audio.transcribe(model, audio_file, prompt=prompt)
+
+        audio_file.close()
+        return {"transcript": transcript}
 
 
 @app.route("/health", methods=["GET"])
